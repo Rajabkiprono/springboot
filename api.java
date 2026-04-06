fd283e39670a18c38b5a1754c614036975e3f651
Rajabkiprono <rajabkiprono5@gmail.com>
2026-04-06 11:51:19 +0300

    Remove unnecessary whitespace and reformat api.java

--- api.java
+++ api.java
@@ -5,20 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users") // All URLs will start with /api/users
+@RequestMapping("/api/users") 
 public class api {
 
-    // This acts as our "Fake Database"
+    
     private List<user> userDatabase = new ArrayList<>();
 
-    // 1. GET all users: http://localhost:8080/api/users
+
     @GetMapping
     public List<user> getAllUsers() {
         return userDatabase;
     }
 
-    // 2. POST a new user: http://localhost:8080/api/users
-    // Use JSON Body: {"name": "Alex", "email": "alex@gmail.com"}
+   
     @PostMapping
     public String createUser(@RequestBody user user) {
         userDatabase.add(user);
